e5957db292c3a6d89e0e4dd42094334531a2d847
Jessica Wang <jessica.l.wang01@gmail.com>
2022-04-02 21:11:44 -0400

    saves data to local storage when save button clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
-// local storage
-// Color code time blocks on past, present, future
+// make sure on refresh it shows
 // ? adjust height of block if overspill?
 // ? Create better array of hours?
-
+// sort array?
 
 // Create array of hours from 9am to 5pm
-var hoursArray = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM'];
+hoursArray = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM'];
 
+eventsArray  = [];
 
 for (i in hoursArray){
   // Adds current date
@@ -16,7 +16,7 @@ for (i in hoursArray){
   // Create each element in a row
   var block = $("<div>")
   .addClass("row block")
-  .attr('id', 'block-'+hoursArray[i])
+  .attr('id', hoursArray[i])
 
   // Create each element in a row
   var hour = $("<h4>")
@@ -25,8 +25,7 @@ for (i in hoursArray){
 
   var events = $("<p>")
   .addClass("block-events col-9")
-  .attr('id', 'events-'+hoursArray[i])
-  .text("hi");
+  .text("");
 
   var save = $("<button>")
   .addClass("block-save col-1");
@@ -45,22 +44,38 @@ for (i in hoursArray){
   currentIndex = hoursArray.indexOf(currentHour);
 
   if (i < currentIndex){
-    $("#block-" + hoursArray[i]).addClass('past')
+    $("#" + hoursArray[i]).addClass('past')
   }else if (i == currentIndex){
-    $("#block-" + hoursArray[i]).addClass('present')
+    $("#" + hoursArray[i]).addClass('present')
   }else{
-    $("#block-" + hoursArray[i]).addClass('future')
+    $("#" + hoursArray[i]).addClass('future')
   }
+
 }
 
 
+var loadEvents = function() {
+  events = JSON.parse(localStorage.getItem("events"));
 
-// for (i in hoursArray){
-//   if (moment().format('HA') == i){
-//     $("block-"+hoursArray[i]).addClass("")
-//   }
+  // if nothing in localStorage, create a new object to track all task status arrays
+  if (!events) {
+    events = {
+    };
+  }
 
-// }
+  // loop over object properties
+  $.each(events, function(list, arr) {
+    // then loop over sub-array
+    arr.forEach(function(event) {
+      createTask(event.text, event.date, list);
+    });
+  });
+};
+
+var saveEvents = function() {
+  localStorage.setItem("eventsArray", JSON.stringify(eventsArray));
+};
+
 
 
 // replace p element with a new textarea
@@ -70,32 +85,62 @@ $("#schedule").on("click", "p", function() {
   var textInput = $("<textarea>").addClass("form-control col-9").val(text);
   $(this).replaceWith(textInput);
   textInput.trigger("focus");
-});
-    
+
+});   
+
 
 // When text area unfocused
 $("#schedule").on("blur", "textarea", function(){
   // get current value of textarea
   var text = $(this).val();
 
-  // get status type and position in the list
-  // var status = $(this)
-  //   .closest(".list-group")
-  //   .attr("id")
-  //   .replace("list-", "");
-  // var index = $(this)
-  //   .closest(".block")
-  //   .index();
-
-  // update task in array and re-save to localstorage
-  // tasks[status][index].text = text;
-  // saveTasks();
-
   // recreate p element
   var events = $("<p>")
     .addClass("block-events col-9")
+
     .text(text);
 
   // replace textarea with new content
   $(this).replaceWith(events);
 });
+
+
+
+$(".block-save").click(function() {
+  hour = $(this).parent().attr('id');
+  eventsText = $("#" + hour + " p").text();
+
+  // Push to eventsArray and save to local storage
+  eventsArray.push({
+    hour: hour,
+    text: eventsText
+  })
+
+  saveEvents();
+
+});
+
+
+
+
+var auditTask = function(taskEl) {
+  // get date from task element
+  var date = $(taskEl)
+    .find("span")
+    .text()
+    .trim();
+
+  // convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+
+  // remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  } else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
+
